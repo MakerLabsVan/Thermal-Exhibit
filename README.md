a3a9ca1113828e028221bb5246fe00fcfbc57db4
Derek Gaw <derekgaw@gmail.com>
2019-06-01 19:46:10 -0700

    Update drawings; add pics to README; refactor some code

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Thermal-Exhibit
 Microcontroller code for the space museum's thermal properties exhibit
 
+Photos in action: https://photos.app.goo.gl/sCPz17kk2VwBpE4D8
+
 Pictures of Arduino wiring:
 ![IMG_0651](https://user-images.githubusercontent.com/51066223/58519778-dbc23d80-8169-11e9-90eb-18799afc12cc.JPG)
 ![IMG_0647](https://user-images.githubusercontent.com/51066223/58519803-f98fa280-8169-11e9-890c-8531c87ad85e.JPG)
--- Thermal_Exhibit.ino
+++ Thermal_Exhibit.ino
@@ -1,8 +1,10 @@
 /*
  * Project: Thermal Properties of Materials Exhibit
- * Description: To display how different materials react under heating. Turns on a heating bulb 
- *              with a push button and heats materials until 40 degrees celsius or until on 30
- *              seconds has passed. Thermistors are used to detect temperature and seven segment
+ * Description: To display how different materials react under heating. 
+                The push button Turns on a heating bulb and heats materials.
+                Bulb shuts off if any of the detected temperature readings are 
+                greater than 40 degress C until 40 degrees celsius or until on 30
+ *              Thermistors are used to detect temperature and seven segment
  *              displays are used to display the temperature.
  * Created: May 29 2019
  */
@@ -21,8 +23,8 @@
 
 
 #define TEST_DELAY   500
-#define MAX_TEMP   30
-#define HEAT_INTERVAL   20000UL
+#define MAX_TEMP   40
+#define HEAT_INTERVAL   200000UL
 
 TM1637Display display1(CLK, DIO1);
 TM1637Display display2(CLK, DIO2);
@@ -54,19 +56,18 @@ void setup() {
   digitalWrite(relayPin, HIGH); // relay off
   pinMode(buttonPin, INPUT_PULLUP);
   previousTime = millis();
-  
 }
 
 void loop() {
   int T1 = getTemperatureInC(ThermistorPin1);
   int T2 = getTemperatureInC(ThermistorPin2);
   int T3 = getTemperatureInC(ThermistorPin3);
-  display1.showNumberDec(T1, false, 2,2);
-  display2.showNumberDec(T2, false, 2,2);
-  display3.showNumberDec(T3, false, 2,2);
+  display1.showNumberDec(T1, false, 2, 2);
+  display2.showNumberDec(T2, false, 2, 2);
+  display3.showNumberDec(T3, false, 2, 2);
   
   if (T1 >= MAX_TEMP || T2 >= MAX_TEMP || T3 >=MAX_TEMP || millis() - previousTime > HEAT_INTERVAL){
-    //Heating bulb will turn off if any material exceeds MAX_TEMP or On-time of bulb has exceeded HEAT_INTERVAL.
+    //Heating bulb will turn off if any material exceeds MAX_TEMP or on-time of bulb has exceeded HEAT_INTERVAL.
     previousTime = millis();
     digitalWrite(relayPin, HIGH); // turn off
   } else if (digitalRead(buttonPin) == HIGH) {
@@ -76,10 +77,12 @@ void loop() {
   delay(TEST_DELAY);
 }
 
+
 //getTermperature function takes inputed voltage value and outputs temperature value in celsuis
+//Equations from: http://www.circuitbasics.com/arduino-thermistor-temperature-sensor-tutorial/
+
 int getTemperatureInC(int pin) {
   int V2 = analogRead(pin);
-  //Equations from: http://www.circuitbasics.com/arduino-thermistor-temperature-sensor-tutorial/
   float R2 = R1 * (1023.0 / (float)V2 - 1.0);  
   float logR2 = log(R2);
   float T3 = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
